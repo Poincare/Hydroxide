c6b71273fe0df9678bb3c0c9bf0fb98ac8c589d1
Dhaivat <dhaivatpandya@gmail.com>
2012-04-08 16:22:12 -0500

    woops, wrong commit

--- examples/clickr/game.js
+++ examples/clickr/game.js
@@ -26,7 +26,7 @@ function createClass() {
 
  		context.beginPath();
 		context.arc(this.centerx, this.centery, 15, 0, 2 * Math.PI, false);
-		context.fillStyle = "#8ED6FF";
+		context.fillStyle = "#ffffff";
 		context.fill();
 		context.closePath(); 
 		//context.fillRect(this.x, this.y, this.width, this.height);
@@ -70,6 +70,22 @@ function createClass() {
   return GameObj;
 }
 
+function createBack() {
+	var b = Object.create(OHGameObj);
+
+	b.x = 0;
+	b.y = 0;
+	b.width = $("#cnv").attr("width");
+	b.height = $("#cnv").attr("height");
+
+	b.draw = function() {
+		context.fillStyle = "#666";
+		context.fillRect(b.x, b.y, b.width, b.width);
+	}
+
+	return b;
+}
+
 function addObject() {
 	var GameObj = createClass();
 	var g = Object.create(GameObj);
@@ -86,7 +102,8 @@ function init() {
   var context = c.getContext("2d");
 
   var GameObj = createClass();
-  
+	var background = createBack();
+ 
 	$("#cnv").click(Hydroxide.mouseClick);
 
 	setInterval("addObject()", 600);
@@ -94,6 +111,7 @@ function init() {
 	var gameStats = {"points":0}
 
 	Hydroxide.registerObject(GameObj);
+	Hydroxide.registerObject(background);
 	Hydroxide.registerDataObject("gameStats", gameStats);
 
   Hydroxide.start("cnv", context, 20, 20, 600, 600);
